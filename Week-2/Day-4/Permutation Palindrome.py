2b7ee1ae34e2c04be07a878488cb937828142891
kadharamkaushik <kadharamkaushik@gmail.com>
2018-07-05 09:16:09 +0530

    05-jul

---
+++ Week-2/Day-4/Permutation Palindrome.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+def has_palindrome_permutation(the_string):
+    # Check if any permutation of the input is a palindrome
+    count=0
+    for i in range(0,len(the_string)//2):
+        if (the_string[i]==the_string(len(the_string)-1-i)):
+           count+=1
+    if(count == len(the_string)//2):
+        return True
+    return False
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcbcd')
+        self.assertTrue(result)
+
+    def test_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabccbdd')
+        self.assertTrue(result)
+
+    def test_no_permutation_with_odd_number_of_chars(self):
+        result = has_palindrome_permutation('aabcd')
+        self.assertFalse(result)
+
+    def test_no_permutation_with_even_number_of_chars(self):
+        result = has_palindrome_permutation('aabbcd')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = has_palindrome_permutation('')
+        self.assertTrue(result)
+
+    def test_one_character_string(self):
+        result = has_palindrome_permutation('a')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
